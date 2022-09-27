9d9c8cb238f057ce11347e18fd2d8179961a6453
Кристина Гуляева <kristinagul.24@gmail.com>
2022-09-27 11:52:56 +1000

    knkjnk

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,27 @@ namespace WpfApp1_Гуляева_Кристина_пр3
         public MainWindow()
         {
             InitializeComponent();
+            a.Text = "4,6";
+            p.Text = "2,9";
+            c.Text = "9,5";
+            Itog.Text = "Ст. гр. ИСП Гуляева, Житкова";
+
+            
+        }
+
+        private void Schet_Click(object sender, RoutedEventArgs e)
+        {
+            Itog.Text += Environment.NewLine + "Практическая работа 2";
+            //  Считывание значения a
+            double a = double.Parse(a.Text);
+            // Вывод значения a в окно
+            Itog.Text += Environment.NewLine + "a = " + a.ToString();
+
+        }
+
+        private void Itog_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
